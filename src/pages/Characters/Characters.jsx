e9ad8e2db211eb3a3987478c5c3c9e85641b5e28
Claire Carden <smclairecarden@gmail.com>
2022-04-26 13:35:51 -0500

    create character list page

--- src/pages/Characters/Characters.jsx
+++ src/pages/Characters/Characters.jsx
@@ -1,14 +1,21 @@
-import { useState } from 'react'
+import React, { useEffect, useState } from 'react'
+import { getCharacters } from '../../services/api-calls';
 
 const Characters = (props) => {
   const [characters, setCharacters] = useState([])
+
+  useEffect(() => {
+    getCharacters()
+    .then(characterData => setCharacters(characterData.data.results))
+  }, [])
+
   return (
     <>
       <div>
         <h1>List of Marvel Characters</h1>
-        {characters.map((characterName) => (
-          <div key={characterName}>
-            {characterName}
+        {characters.map((character) => (
+          <div key={character.name}>
+            {character.name}
           </div>
         ))}
       </div>
---
+++ src/services/api-calls.js
@@ -0,0 +1,6 @@
+const baseURL = "https://gateway.marvel.com/v1/public/"
+
+export function getCharacters() {
+  return fetch(`${baseURL}characters?limit=100&offset=550&ts=${process.env.REACT_APP_TS}&apikey=${process.env.REACT_APP_API_KEY}&hash=${process.env.REACT_APP_HASH}`)
+  .then(res => res.json())
+}
