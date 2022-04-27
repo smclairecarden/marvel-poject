8c4527c1358da1694e24858138aa785bdb5a9ce7
Claire Carden <smclairecarden@gmail.com>
2022-04-27 12:23:27 -0500

    search functionality complete

--- src/pages/Characters/Characters.jsx
+++ src/pages/Characters/Characters.jsx
@@ -1,27 +1,42 @@
-import React, { useEffect, useState } from 'react'
-import { getCharacters } from '../../services/api-calls';
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
+import SearchForm from '../../components/SearchForm/SearchForm';
+import { characterSearch } from '../../services/api-calls';
 
 const Characters = (props) => {
   const [characters, setCharacters] = useState([])
 
-  useEffect(() => {
-    getCharacters()
-    .then(characterData => setCharacters(characterData.data.results))
-  }, [])
+
+  // useEffect(() => {
+  //   getCharacters()
+  //   .then(characterData => setCharacters(characterData.data.results))
+  // }, [])
+
+  const handleCharacterSearch = formData => {
+    characterSearch(formData)
+    .then(characterResults => {
+      setCharacters(characterResults.data.results)
+    })
+  }
 
   return (
     <>
       <div>
-        <h1>List of Marvel Characters</h1>
-        {characters.map((character) => (
+        <h1>Marvel Characters</h1>
+        < SearchForm handleCharacterSearch={handleCharacterSearch} />
+        {characters.length ?
+        <>
+        {characters?.map(character => 
           <div key={character.name}>
             <Link to="/details" state={{character}}>
               {character.name}
             </Link>
           </div>
-          
-        ))}
+        )}
+        </>
+        :
+        <h3>Please search for a character!</h3>
+        }
       </div>
     </>
   );
--- src/services/api-calls.js
+++ src/services/api-calls.js
@@ -5,6 +5,11 @@ export function getCharacters() {
   .then(res => res.json())
 }
 
+export function characterSearch(formData) {
+  return fetch(`${baseURL}characters?name=${formData.query}&limit=20&ts=${process.env.REACT_APP_TS}&apikey=${process.env.REACT_APP_API_KEY}&hash=${process.env.REACT_APP_HASH}`)
+  .then(res => res.json())
+}
+
 export function getCharacterDetails(Id) {
   return fetch(`${baseURL}characters/${Id}?ts=${process.env.REACT_APP_TS}&apikey=${process.env.REACT_APP_API_KEY}&hash=${process.env.REACT_APP_HASH}`)
   .then(res => res.json())
