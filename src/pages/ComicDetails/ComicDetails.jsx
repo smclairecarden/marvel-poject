f4c9cbe825811ac2c270ec19618b3a70b30c85bc
Claire Carden <smclairecarden@gmail.com>
2022-04-27 19:31:06 -0500

    photos on comic page

--- src/pages/ComicDetails/ComicDetails.jsx
+++ src/pages/ComicDetails/ComicDetails.jsx
@@ -4,23 +4,33 @@ import { useLocation, Link } from 'react-router-dom';
 
 const ComicDetails = (props) => {
   const [comicDetails, setComicDetails] = useState([])
-  // const [photo, setPhoto] = useState({})
+  const [photo, setPhoto] = useState({})
   let location = useLocation()
 
   useEffect(() => {
+    console.log(location.state.comic.images)
     getComicDetails(location.state.comic.id)
     .then(comic => setComicDetails(comic.data.results[0]))
+    
   }, [])
 
-  // useEffect(() => {
-  //   setPhoto(`${location.state.character.thumbnail.path}/portrait_incredible.jpg`)
-  // }, [])
+  useEffect(() => {
+    setPhoto(`${location.state.comic.images[0]?.path}/portrait_incredible.jpg`)
+  }, [])
 
   return (
     <>
       <div key={comicDetails.title}>
         <>
         <h1>{comicDetails.title}</h1>
+        {photo.length > 0 ?
+        <>
+        
+        <img src={photo} alt="comic" />
+        </>
+        :
+        <p></p>
+        }
         <p>{comicDetails.description}</p>
         <p>{comicDetails.issueNumber}</p>
         <Link to="/comics" >
